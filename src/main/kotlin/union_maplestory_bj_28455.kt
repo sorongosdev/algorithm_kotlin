02fc7c812f0f2dd8aa5d75e2e8ae6431a9883a3f
Sorongos <dosora9@gmail.com>
2025-01-05 19:05:47 +0900

    [브2] 백준 28455번: Union Maplestory

---
+++ src/main/kotlin/union_maplestory_bj_28455.kt
@@ -0,0 +1,29 @@
+package main.kotlin
+
+fun main() {
+    val N = readLine()!!.toInt()
+    val levelList = mutableListOf<Int>()
+
+    val xpList = listOf(60, 100, 140, 200, 250)
+
+    repeat(N) {
+        levelList.add(readLine()!!.toInt())
+    }
+
+    val sortedLevelList = levelList.sortedDescending().take(minOf(N, 42))
+
+    var levelSum = sortedLevelList.sum()
+    var xpSum = 0
+
+    // get xpSum
+    xpList.forEach { xp ->
+        sortedLevelList.forEach{ L ->
+            if(L >= xp){
+                xpSum += 1
+            }
+        }
+    }
+
+
+    print("$levelSum $xpSum")
+}
