27ad6348aeb8ad258d21a5bfc5947350705739d6
sorongosdev <dosora9@gmail.com>
2025-01-07 18:03:22 +0900

    [실5] 백준 10814번: 나이순 정렬⚠️

---
+++ src/main/kotlin/age_sort_bj_10814.kt
@@ -0,0 +1,19 @@
+package src.main.kotlin
+
+fun main() {
+    val peopleNum = readln().toInt()
+    val peopleList = mutableListOf<Pair<Int, String>>()
+
+    repeat(peopleNum) {
+        val (age, name) = readln().split(" ")
+        peopleList.add(Pair(age.toInt(), name))
+    }
+
+    // peopleList의 age에 따라 아이템 정렬, 같은 값이면 입력순서 유지(stable sort)
+    val peopleSortedList = peopleList.sortedBy { it.first }
+
+    peopleSortedList.forEach { (age, name) ->
+        // key와 value를 스페이스로 구분해 출력
+        println("$age $name")
+    }
+}
